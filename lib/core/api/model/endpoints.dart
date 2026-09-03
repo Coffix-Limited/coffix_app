46fb4f429f3e79ebcebe55192d319e3df201da9b
edilberto-pajunar <espajunarjr@gmail.com>
2026-09-03 14:20:11 +0800

    update endpoint

--- lib/core/api/model/endpoints.dart
+++ lib/core/api/model/endpoints.dart
@@ -3,8 +3,5 @@ import 'package:flutter/foundation.dart';
 
 abstract class ApiEndpoints {
   static String get endpoint => FlavorConfig.instance.baseUrl;
-  static String get v1 => kDebugMode
-      // ? "http://127.0.0.1:5001/coffix-app-dev/us-central1/v1"
-      ? "https://amenity-demanding-carol.ngrok-free.dev/coffix-app-dev/us-central1/v1"
-      : endpoint;
+  static String get v1 => kDebugMode ? "http://127.0.0.1:5001/coffix-app-dev/us-central1/v1" : endpoint;
 }
